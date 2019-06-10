d6d566b0cae2c87e4e567ae848b57741cfead062
k.villatel <k.villatel@k.villatel-laptop>
2019-06-10 17:26:44 +0200

    Add some comments and traces

--- mlflow_project_model.py
+++ mlflow_project_model.py
@@ -8,7 +8,8 @@ import click
 
 @click.command()
 @click.option("--seed", default=0, help="Seed for the experiment")
-def main(seed):
+@click.option("--max_iter", default=100, help="Number of iterations")
+def main(seed, max_iter):
   # Set the tensorflow experiment
   mlflow.set_experiment("reproductible_experiment")
 
@@ -18,11 +19,13 @@ def main(seed):
 
     # Log params
     mlflow.log_param("seed", seed)
+    mlflow.log_param("max_iter", max_iter)
 
     # Set seed
     np.random.seed(seed)
 
     # Download iris dataset
+    print("Using Iris dataset")
     iris = datasets.load_iris()
 
     X = iris.data
@@ -32,15 +35,20 @@ def main(seed):
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     # Train model
-    classifier = LogisticRegression(multi_class="multinomial", solver="lbfgs")
+    print("Training multinomial logistic regression model")
+    classifier = LogisticRegression(multi_class="multinomial", solver="lbfgs", max_iter=max_iter)
 
     classifier.fit(X, y)
 
     # Log model artifact
     mlflow.sklearn.log_model(classifier, "model")
 
+    acc = classifier.score(X_test, y_test)
+
     # Log metric
-    mlflow.log_metric("acc", classifier.score(X_test, y_test))
+    mlflow.log_metric("acc", acc)
+
+    print("Score on test set", acc)
 
 
 if __name__ == "__main__":
